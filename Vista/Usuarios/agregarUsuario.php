ba81d13705d67bde95fc2f1d050048d8701df91f
David <UTTI192054@utvco.edu.mx>
2023-03-24 11:56:49 -0600

    Corrección de interfaz usuario

--- Vista/Usuarios/agregarUsuario.php
+++ Vista/Usuarios/agregarUsuario.php
@@ -26,8 +26,6 @@
                                                 <div class="section-intro text-center">
                                                     Este tipo de usuario tendrá que registrar su correo electronico y
                                                     asignar una contraseña para poder ingresar al sistema.
-                                                    Este es una prueba.
-                                                    Este es una prueba 2
                                                 </div>
                                             </div>
                                             <div class="col-12 col-md-10">
